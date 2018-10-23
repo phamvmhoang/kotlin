1a6b5f328f0131e35cb5d9df41c6d36f8319e06e
hoangpvm <hoangpvm.dev@gmail.com>
2018-10-24 00:40:59 +0700

    Add Kotlin Maven Sample

---
+++ kotlin-quick-start-guide/introduction-kotlin/kotlin-maven-app/src/main/kotlin/hoang/pham/vm/Hello.kt
@@ -0,0 +1,6 @@
+package hoang.pham.vm
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ kotlin-quick-start-guide/introduction-kotlin/kotlin-maven-app/src/test/kotlin/hoang/pham/vm/HelloTest.kt
@@ -0,0 +1,8 @@
+package hoang.pham.vm
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
